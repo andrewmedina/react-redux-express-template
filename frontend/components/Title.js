c461c48f762dd719d84e5c9248be7537e64024cf
pdelancy <pdelancy94@gmail.com>
2017-10-21 01:21:43 -0700

    stuff

--- frontend/components/Title.js
+++ frontend/components/Title.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+
+const clickHandler = () => {
+  console.log("in clickHandler");
+  axios.get("https://turntableapp.herokuapp.com/auth/spotify")
+  .then((resp)=>res.send(resp))
+}
 
 const Title = ( { name } ) => {
+  console.log("in title");
     return (
-        <h1>{name}</h1>
-    );
+      <div>
+        <button onClick={()=>(clickHandler())}>Login with Spotify</button>
+      </div>
+
+          );
 };
 
 Title.propTypes = {
